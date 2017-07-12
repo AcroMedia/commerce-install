e57f3bf77482e860ef8ee5e823d1641b26170607
Dave Nelson <dnelson@acromediainc.com>
2017-07-12 08:42:22 -0700

    progress on component stuff

--- js/script.js
+++ js/script.js
@@ -4,87 +4,107 @@
 
 $(document).ready(function ($) {
   $('.arrow_up').on('click', function(e) {
-     $(this).toggleClass('down').siblings().removeClass('down');
+    $(this).toggleClass('down').siblings().removeClass('down');
 
-      $(this).parent().toggleClass('show').siblings().removeClass('show');
-      e.preventDefault();
-      $('.product__build').each(function(){
-        if($(this).hasClass('show')) {
-            $(this).closest('.product__build').children('.product__content').toggleClass('off',800).siblings().removeClass('off');
-        }
-        else {
-          $(this).closest('.product__build').children('.product__content').removeClass('off');
-        }
-      });
+    $(this).parent().toggleClass('show').siblings().removeClass('show');
+    e.preventDefault();
+    $('.product__build').each(function(){
+      if($(this).hasClass('show')) {
+        $(this).closest('.product__build').children('.product__content').toggleClass('off',800).siblings().removeClass('off');
+      }
+      else {
+        $(this).closest('.product__build').children('.product__content').removeClass('off');
+      }
+    });
   });
 
   Vue.component('kickstart-section', {
-      props: ['title', 'description', 'iconClass', 'options', 'activeIndex', 'options', 'selectSection'],
-      template: '#kickstart-section-template',
+    props: ['title', 'description', 'iconClass', 'options', 'activeIndex', 'options'],
+    template: '#kickstart-section-template',
   });
 
-  Vue.component('card', {
-    template: '\
-    <div class="box__item column" v-on:click="greet">\
-    <div v-if="item.image" class="pay__logo">\
-    <img :src="item.image_src" alt="">\
-    </div>\
-      <h3>{{ item.title }}</h3>\
-      <div class="box__description">\
-      {{item.description}}\
-      </div>\
-    </div>\
-  ',
-    props: ['item']
+  Vue.component('kickstart-options', {
+    props: ['options', 'activeIndex', 'section'],
+    template: '#kickstart-options-template',
+    methods: {
+      emit: function(index, section) {
+        this.$emit('option_click', {index: index, section: section});
+      }
+    }
   });
 
-    new Vue({
-        el: '#app',
-        data: {
-            locations: {
-                options: [
-                    { id: 0, title: 'International', description: 'Do we need a description with these things?'},
-                    { id: 1, title: 'North America', description: 'Do we need a description with these things?'},
-                    { id: 2, title: 'EU', description: 'Do we need a description with these things?'},
-                    { id: 3, title: 'USA', description: 'Do we need a description with these things?'}
-                ],
-            },
-            packages: {
-                options: [
-                    {id: 0, title: 'Base', description: 'Do we need a description with these things?'},
-                    {id: 1, title: 'Catalog', description: 'Do we need a description with these things?'},
-                    {id: 2, title: 'Events', description: 'Do we need a description with these things?'},
-                    {id: 3, title: 'Subscriptions', description: ''}
-                ]
-            },
-            payments: {
-                options: [
-                    { id: 0, title: 'Paypal', description: 'Do we need a description with these things?', image: true, image_src: './gfx/paylogos/paypal.png'},
-                    { id: 1, title: 'BrainTree', description: 'Do we need a description with these things?', image: true, image_src: './gfx/paylogos/braintree.png'},
-                    { id: 3, title: 'Apple Pay', description: 'Do we need a description with these things', image: true, image_src: './gfx/paylogos/apple-pay.png'},
-                    { id: 2, title: 'Auth.net', description: 'Do we need a description with these things?', image: true, image_src: './gfx/paylogos/authorize.png'},
-                    { id: 4, title: 'Moneris', description: 'Do we need a description with these things'},
-                    { id: 5, title: 'Pay With Chickens', description: 'Do we need a description with these things'}
-                ]
-            },
-            contents: {
-                options: [
-                    {id: 0, title: 'Migrate', description: 'Do we need a description with these things?'},
-                    {id: 1, title: 'Demo Content', description: 'Do we need a description with these things?'},
-                    {id: 2, title: 'Clean', description: 'Do we need a description with these things?'}
-                ]
-            }
+  var app = new Vue({
+    el: '#app',
+    data: {
+      sections: {
+        locations: {
+          title: "Locations",
+          description: "Explain stuff good... ok? commmerce is an open source Adaptive Sales Platfasily configured and integrated into the tools that drive your business today and in the future.",
+          options: [
+            {id: 0, title: 'International', description: 'Do we need a description with these things?'},
+            {id: 1, title: 'North America', description: 'Do we need a description with these things?'},
+            {id: 2, title: 'EU', description: 'Do we need a description with these things?'},
+            {id: 3, title: 'USA', description: 'Do we need a description with these things?'}
+          ],
+          activeIndex: false,
         },
-        // define methods under the `methods` object
-        methods: {
-            greet: function (event) {
-                // `this` inside methods points to the Vue instance
-                alert('Hello ' + this.name + '!')
-                // `event` is the native DOM event
-                if (event) {
-                    alert(event.target.tagName)
-                }
-            }
+        packages: {
+          options: [
+            {id: 0, title: 'Base', description: 'Do we need a description with these things?'},
+            {id: 1, title: 'Catalog', description: 'Do we need a description with these things?'},
+            {id: 2, title: 'Events', description: 'Do we need a description with these things?'},
+            {id: 3, title: 'Subscriptions', description: ''}
+          ]
+        },
+        payments: {
+          options: [
+            {
+              id: 0,
+              title: 'Paypal',
+              description: 'Do we need a description with these things?',
+              image: true,
+              image_src: './gfx/paylogos/paypal.png'
+            },
+            {
+              id: 1,
+              title: 'BrainTree',
+              description: 'Do we need a description with these things?',
+              image: true,
+              image_src: './gfx/paylogos/braintree.png'
+            },
+            {
+              id: 3,
+              title: 'Apple Pay',
+              description: 'Do we need a description with these things',
+              image: true,
+              image_src: './gfx/paylogos/apple-pay.png'
+            },
+            {
+              id: 2,
+              title: 'Auth.net',
+              description: 'Do we need a description with these things?',
+              image: true,
+              image_src: './gfx/paylogos/authorize.png'
+            },
+            {id: 4, title: 'Moneris', description: 'Do we need a description with these things'},
+            {id: 5, title: 'Pay With Chickens', description: 'Do we need a description with these things'}
+          ]
+        },
+        contents: {
+          options: [
+            {id: 0, title: 'Migrate', description: 'Do we need a description with these things?'},
+            {id: 1, title: 'Demo Content', description: 'Do we need a description with these things?'},
+            {id: 2, title: 'Clean', description: 'Do we need a description with these things?'}
+          ]
         }
-    });
+      },
+      summary: []
+    },
+    methods: {
+      selectOption: function(index) {
+        console.log('a test');
+        console.log(index);
+      }
+    }
+  });
 });
