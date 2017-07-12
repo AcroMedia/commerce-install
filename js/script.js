30c99309d2c4f040eefa97648efe2cd5a78feae0
Dave Nelson <dnelson@acromediainc.com>
2017-07-12 11:29:34 -0700

    some refactoring to put everything in a global object

--- js/script.js
+++ js/script.js
@@ -1,6 +1,71 @@
 
 
-
+var cart = {items: {}};
+var sections = {
+  locations: {
+    title: "Locations",
+    description: "Explain stuff good... ok? commmerce is an open source Adaptive Sales Platfasily configured and integrated into the tools that drive your business today and in the future.",
+    options: [
+      {id: 0, title: 'International', description: 'Do we need a description with these things?'},
+      {id: 1, title: 'North America', description: 'Do we need a description with these things?'},
+      {id: 2, title: 'EU', description: 'Do we need a description with these things?'},
+      {id: 3, title: 'USA', description: 'Do we need a description with these things?'}
+    ],
+    activeIndex: false,
+  },
+  packages: {
+    options: [
+      {id: 0, title: 'Base', description: 'Do we need a description with these things?'},
+      {id: 1, title: 'Catalog', description: 'Do we need a description with these things?'},
+      {id: 2, title: 'Events', description: 'Do we need a description with these things?'},
+      {id: 3, title: 'Subscriptions', description: ''}
+    ],
+    activeIndex: false,
+  },
+  payments: {
+    options: [
+      {
+        id: 0,
+        title: 'Paypal',
+        description: 'Do we need a description with these things?',
+        image: true,
+        image_src: './gfx/paylogos/paypal.png'
+      },
+      {
+        id: 1,
+        title: 'BrainTree',
+        description: 'Do we need a description with these things?',
+        image: true,
+        image_src: './gfx/paylogos/braintree.png'
+      },
+      {
+        id: 3,
+        title: 'Apple Pay',
+        description: 'Do we need a description with these things',
+        image: true,
+        image_src: './gfx/paylogos/apple-pay.png'
+      },
+      {
+        id: 2,
+        title: 'Auth.net',
+        description: 'Do we need a description with these things?',
+        image: true,
+        image_src: './gfx/paylogos/authorize.png'
+      },
+      {id: 4, title: 'Moneris', description: 'Do we need a description with these things'},
+      {id: 5, title: 'Pay With Chickens', description: 'Do we need a description with these things'}
+    ],
+    activeIndex: false,
+  },
+  contents: {
+    options: [
+      {id: 0, title: 'Migrate', description: 'Do we need a description with these things?'},
+      {id: 1, title: 'Demo Content', description: 'Do we need a description with these things?'},
+      {id: 2, title: 'Clean', description: 'Do we need a description with these things?'}
+    ],
+    activeIndex: false,
+  }
+};
 
 $(document).ready(function ($) {
   $('.arrow_up').on('click', function(e) {
@@ -23,13 +88,29 @@ $(document).ready(function ($) {
     template: '#kickstart-section-template',
   });
 
-  Vue.component('kickstart-options', {
-    props: ['options', 'activeIndex', 'section'],
+  Vue.component('cards', {
+    props: ['section'],
+    data: function() {
+      return {
+        options: sections[this.section].options,
+        activeIndex: sections[this.section].activeIndex
+      };
+    },
     template: '#kickstart-options-template',
     methods: {
       emit: function(index, section) {
-        this.$emit('option_click', {index: index, section: section});
+        console.log(sections[section]);
+        Vue.set(cart.items, section, {section: section, index: index});
+        Vue.set(sections[section], 'activeIndex', index);
+        this.activeIndex = index;
       }
+    }
+  });
+
+  Vue.component('cart-summary', {
+    template: '<div id="a-tester"><div class="cart-item" v-for="item in items">{{ item.section }}: {{ item.index }}</div></div>',
+    data: function () {
+      return cart;
     }
   });
 
@@ -54,7 +135,8 @@ $(document).ready(function ($) {
             {id: 1, title: 'Catalog', description: 'Do we need a description with these things?'},
             {id: 2, title: 'Events', description: 'Do we need a description with these things?'},
             {id: 3, title: 'Subscriptions', description: ''}
-          ]
+          ],
+          activeIndex: false,
         },
         payments: {
           options: [
@@ -88,14 +170,16 @@ $(document).ready(function ($) {
             },
             {id: 4, title: 'Moneris', description: 'Do we need a description with these things'},
             {id: 5, title: 'Pay With Chickens', description: 'Do we need a description with these things'}
-          ]
+          ],
+          activeIndex: false,
         },
         contents: {
           options: [
             {id: 0, title: 'Migrate', description: 'Do we need a description with these things?'},
             {id: 1, title: 'Demo Content', description: 'Do we need a description with these things?'},
             {id: 2, title: 'Clean', description: 'Do we need a description with these things?'}
-          ]
+          ],
+          activeIndex: false,
         }
       },
       summary: []
