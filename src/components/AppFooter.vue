e04164ab3f0fdcc969a839487fb26d488532c938
Hubbs <mhubbard@acromediainc.com>
2017-09-15 10:23:16 -0700

    Update footer layout and styles.

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,5 +1,19 @@
 <template>
   <footer class="app-footer">
+     <div class="demo__content">
+       <div class="container">
+         <a href="https://commercedemo.acromedia.com" class="demo__link" target="_blank" title="Click to view a Drupal Commerce live demo">
+           <div class="demo__screenshot">
+             <img src="../assets/gfx/urban-hipster-screenshot.png" alt="Drupal Commerce demo site screenshot">
+           </div>
+           <div class="demo__info">
+              <h3>Drupal Commerce - Live Demo</h3>
+              Check out this live out-of-the-box demo of Drupal Commerce.
+           </div>
+         </a>
+       </div>
+    </div>
+
     <div class="portfolio__content">
       <h3>Who uses Drupal Commerce?</h3>
       <div class="footer__logos container">
@@ -13,50 +27,54 @@
     </div>
 
     <div class="footer__section container">
-      <div class="brand__logos">
-        <div class="columns">
-          <div class="column">
-            <a href="#">
-              <div class="commerce-guy">
-                <img src="../assets/gfx/commerce-guy-logo.png" alt="">
-              </div>
-              <div class="logo__slogan">
-                Creators of<br>
-                Drupal Commerce
+      <div class="brand__logos columns">
+        <div class="column">
+          <div class="commerce-guys">
+            <div class="commerce-guys__logo">
+              <img src="../assets/gfx/commerce-guy-logo.png" alt="">
+            </div>
+            <div class="brand__slogan">
+              Creators of<br>
+              Drupal Commerce
+            </div>
+            <div class="brand__info">
+              Giving industry leaders proident molestiae lobortis incidunt error, pariatur conubia beatae nulla, esse tempor lorem pretium lacus cursus
+            </div>
+            <div class="brand__links">
+              <a href="https://www.commerceguys.com" class="web-icon" target="_blank" title="Go to the Commerce Guys' website">
+                commerceguys.com
+              </a>
+              <a href="mailto:sales@commerceguys.com" class="email-icon" target="_blank" title="Email sales@commerceguys.com">
+                sales@commerceguys.com
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="acro-media">
+            <div class="acro-media__logo">
+              <img src="../assets/gfx/acromedia-logo.png" alt="">
+            </div>
+            <div class="brand__slogan">
+              North America's #1<br>
+              Drupal Commerce Provider
+            </div>
+            <div class="brand__info">
+              Giving industry leaders proident molestiae lobortis incidunt error, pariatur conubia beatae nulla, esse tempor lorem pretium lacus cursus
+            </div>
+            <div class="brand__links">
+              <a href="https://www.acromedia.com" class="web-icon" target="_blank" title="Go to Acro Media's website">
+                acromedia.com
+              </a>
+              <a href="mailto:solutions@acromediainc.com" class="email-icon" target="_blank" title="Email solutions@acromediainc.com">
+                solutions@acromediainc.com
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
 
-              </div>
-            </a>
-          </div>
-          <div class="column">
-            <a href="#">
-              <div class="acromedia">
-                <img src="../assets/gfx/acromedia-logo.png" alt="">
-              </div>
-              <div class="logo__slogan">
-                North America's #1<br>
-                Drupal Commerce Provider
-              </div>
-            </a>
-          </div>
-        </div>
-      </div>
-      <div class="contact__section">
-        <div class="site-url__block">
-          commerceguys.com
-        </div>
-        <div class="email__block">
-          sales@commerceguys.com
-        </div>
-        <div class="divider">&nbsp;|&nbsp;</div>
-        <div class="site-url__block">
-          acromedia.com
-        </div>
-        <div class="email__block">
-          solution@acromediainc.com
-        </div>
-      </div>
       <div class="footer__copyright">
-
         © 1998 - 2017 <a href="#">Acro Media Inc.</a> <a href="#">Privacy Policy </a>
       </div>
     </div>
